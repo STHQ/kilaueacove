0d25909af835f60ca09722ac8a6e2aadda1c1174
Mark Boszko <mark@boszko.com>
2016-02-29 20:59:35 -0800

    First commit
    FIXME: Does not actually make the SuperPixel type yet. This is just for
    testing.

---
+++ tikinook/superpixel.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import time
+
+import neopixel
+import paleopixel
+
+# SuperPixel
+# Author:  Mark Boszko
+#
+# Combines both NeoPixels and PaleoPixels (WS2801) into a single addressable
+# SuperPixel type. Also allows merging of separate pixel strands into one
+# master strand or array.
+
+
+# My LED strip configurations (for test):
+NEOPIXEL_COUNT   = 244   # Number of NeoPixels in the strand
+NEOPIXEL_PIN     = 18    # GPIO pin connected to the pixels (must support PWM!)
+PALEOPIXEL_COUNT = 50    # Number of PaleoPixels in the strand
+
+
+
+
+# Main program logic follows:
+if __name__ == '__main__':
+	# Create NeoPixel object with appropriate configuration.
+	strip1 = Adafruit_NeoPixel(NEOPIXEL_COUNT, NEOPIXEL_PIN)
+	strip2 = PaleoPixel(PALEOPIXEL_COUNT)
+	# Intialize the library (must be called once for NeoPixels before other functions).
+	strip1.begin()
+
+	print 'Press Ctrl-C to quit.'
+	while True:
+		# Color wipe animations.
+		colorWipe(strip1, Color(255, 0, 0))  # Red wipe
+		colorWipe(strip2, Color(255, 0, 0))  # Red wipe
+		colorWipe(strip1, Color(0, 255, 0))  # Blue wipe
+		colorWipe(strip2, Color(0, 255, 0))  # Blue wipe
+		colorWipe(strip1, Color(0, 0, 255))  # Green wipe
+		colorWipe(strip2, Color(0, 0, 255))  # Green wipe
+		# Theater chase animations.
+		theaterChase(strip1, Color(127, 127, 127))  # White theater chase
+		theaterChase(strip2, Color(127, 127, 127))  # White theater chase
+		theaterChase(strip1, Color(127,   0,   0))  # Red theater chase
+		theaterChase(strip2, Color(127,   0,   0))  # Red theater chase
+		theaterChase(strip1, Color(  0,   0, 127))  # Blue theater chase
+		theaterChase(strip2, Color(  0,   0, 127))  # Blue theater chase
+		# Rainbow animations.
+		rainbow(strip1)
+		rainbow(strip2)
+		rainbowCycle(strip1)
+		rainbowCycle(strip2)
+		theaterChaseRainbow(strip1)
+		theaterChaseRainbow(strip2)
